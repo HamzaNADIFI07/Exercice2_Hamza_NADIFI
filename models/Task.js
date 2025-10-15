f621fc54c42609be5cfbc769fba990b80e0dbdcf
Hamza NADIFI <nadifihamza07@gmail.com>
2025-10-15 16:33:11 +0200

    delete Task.js functions

--- models/Task.js
+++ models/Task.js
@@ -3,19 +3,5 @@ export class Task {
       this.id = id;
       this.title = title;
     }
-  
-    to_dict() {
-      return {
-        id: this.id,
-        title: this.title,
-      };
-    }
-  
-    static from_dict(data) {
-      return new Task({
-        title: data.title,
-        id: data.id,
-      });
-    }
   }
   
