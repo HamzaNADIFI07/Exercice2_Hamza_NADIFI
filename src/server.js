d8d01000c9719ad46b2387e9495df5ed678273e7
Hamza NADIFI <nadifihamza07@gmail.com>
2025-10-17 16:24:42 +0200

    updater server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import tasksRouter from "./routes/taskRoutes.js";
 
@@ -6,13 +6,19 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/", tasksRouter);
 
-app.use((req, res) => res.status(404).json({ error: "Not Found", path: req.path }));
-app.use((err, _req, res, _next) => { console.error(err); res.status(500).json({ error: "Erreur serveur" }); });
+// 404
+app.use((req, res) =>
+  res.status(404).json({ error: "Not Found", path: req.path })
+);
+
+// 500
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  res.status(500).json({ error: "Erreur serveur" });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://127.0.0.1:${PORT}`);
+  console.log(`API prête → http://127.0.0.1:${PORT}`);
 });
