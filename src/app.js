a4b710527ab8a1615294f9fe210d45cb8de75eb8
Hamza NADIFI <nadifihamza07@gmail.com>
2025-11-06 16:35:07 +0100

    add Swagger docs and UI at /api-docs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import express from "express";
 import tasksRouter from "./routes/taskRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("./swagger.json");
+
 
 const app = express();
 
 app.use(express.json());
 app.use("/", tasksRouter);
 
+app.use('/api-docs' , swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // 404
 app.use((req, res) =>
   res.status(404).json({ error: "Not Found", path: req.path })
