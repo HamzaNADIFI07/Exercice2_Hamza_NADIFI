5211282eb4cbb1c1191a7957e8ce4734c7182796
Hamza NADIFI <nadifihamza07@gmail.com>
2025-11-10 15:09:28 +0100

    update app.js file with Authentification routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
 import "dotenv/config";
 import express from "express";
-import tasksRouter from "./routes/taskRoutes.js";
+import helmet from "helmet";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-const swaggerDocument = require("./swagger.json");
+import cookieParser from 'cookie-parser';
+import authRouter from "./routes/authRoutes.js";
+import tasksRouter from "./routes/taskRoutes.js";
+import { requireAuth } from "./config/auth.js";
 
+import swaggerDocument from "./swagger.json" with { type: "json" };
 
 const app = express();
 
+// Sécurité
+app.use(helmet());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use("/", tasksRouter);
+app.use(cookieParser());
 
-app.use('/api-docs' , swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// Health
+app.get("/health", (_req, res) => {
+  res.json({ status: "ok", message: "API up" });
+});
+
+
+
+// Routes
+app.use("/auth", authRouter); // /auth/register, /auth/login
+app.use("/api",requireAuth, tasksRouter); // /api/tasks (protégé)
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // 404
 app.use((req, res) =>
   res.status(404).json({ error: "Not Found", path: req.path })
@@ -21,7 +40,10 @@ app.use((req, res) =>
 // 500
 app.use((err, _req, res, _next) => {
   console.error(err);
-  res.status(500).json({ error: "Erreur serveur" });
+  const status = err.status || 500;
+  res.status(status).json({
+    error: err.message || "Erreur serveur",
+  });
 });
 
 export default app;
